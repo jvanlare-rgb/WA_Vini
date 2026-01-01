d9f55af3c92c61674e5b1d44c5992147fd1f5da6
Jay Van Lare <jvanlare@uw.edu>
2026-01-01 11:05:27 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@ mapboxgl.accessToken = "pk.eyJ1IjoianZhbmxhcmUiLCJhIjoiY21oY2Zrd29nMTN2dDJtcHh5Y
 
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/outdoors-v12",
+  style: "mapbox://styles/jvanlare/cmjuj1tdb000z01s48ibtg7t1",
   center: [-119.0, 46.8],
   zoom: 6
 });
-
-
-// Helpful: surface errors in console
-map.on("error", (e) => console.log("MAP ERROR:", e?.error || e));
-map.on("load", () => console.log("✅ style loaded"));
