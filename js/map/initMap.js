736ced3333cfef8c8068243d3ddb2c71cd2851f9
Jay Van Lare <jvanlare@uw.edu>
2026-01-02 08:55:39 -0800

    Implement initMap function for Mapbox integration

--- js/map/initMap.js
+++ js/map/initMap.js
@@ -1,1 +1,14 @@
+// js/map/initMap.js
+export function initMap(mapboxgl, config) {
+  mapboxgl.accessToken = config.accessToken;
+
+  const map = new mapboxgl.Map({
+    container: config.map.container,
+    style: config.map.style,
+    center: config.map.center,
+    zoom: config.map.zoom
+  });
+
+  return map;
+}
 
