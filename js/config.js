d0666f0e4b5793f38d9fc3135ba789791d0f4707
Jay Van Lare <jvanlare@uw.edu>
2026-01-02 08:42:29 -0800

    Update accessToken in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  accessToken: "YOUR_TOKEN",
+  accessToken: "pk.eyJ1IjoianZhbmxhcmUiLCJhIjoiY21oY2Zrd29nMTN2dDJtcHh5YzlxYWVtNSJ9.bP5BGQT-tdmmsC1SStqvNw",
   style: "mapbox://styles/jvanlare/....",
   avaUrl: "./data/avas_wa.geojson"
 };
