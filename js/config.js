877bc73d13504f58d33018a83807a4d25342da54
Jay Van Lare <jvanlare@uw.edu>
2026-01-02 08:55:23 -0800

    Restructure CONFIG object in config.js
    Refactor CONFIG object structure for better organization.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,22 @@
+// js/ui/config.js
 export const CONFIG = {
-  accessToken: "pk.eyJ1IjoianZhbmxhcmUiLCJhIjoiY21oY2Zrd29nMTN2dDJtcHh5YzlxYWVtNSJ9.bP5BGQT-tdmmsC1SStqvNw",
-  style: "mapbox://styles/jvanlare/....",
-  avaUrl: "./data/avas_wa.geojson"
+  accessToken:
+    "pk.eyJ1IjoianZhbmxhcmUiLCJhIjoiY21oY2Zrd29nMTN2dDJtcHh5YzlxYWVtNSJ9.bP5BGQT-tdmmsC1SStqvNw",
+
+  map: {
+    container: "map",
+    style: "mapbox://styles/jvanlare/cmjuj1tdb000z01s48ibtg7t1",
+    center: [-119.0, 46.8],
+    zoom: 6
+  },
+
+  data: {
+    AVA_URL: "./data/avas_wa.geojson"
+  },
+
+  ids: {
+    SOURCE_ID: "ava",
+    FILL_ID: "ava-fill",
+    OUTLINE_ID: "ava-outline"
+  }
 };
